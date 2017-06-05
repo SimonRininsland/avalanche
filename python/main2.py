9ff6057efcf63f2c6fc7f47f021b95feccfda1db
Tiras Zemicael <Tiras.Zemicael@gmail.com>
2017-06-05 23:24:21 +0200

    copy of main with key event handler and mouse event handler

---
+++ python/main2.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from pygame import draw
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+width, height = (1280, 720)
+
+# ASCII OCTAL for ESCAPE
+ESCAPE = '\033'
+
+
+
+def drawLoop():
+    # display all the stuff
+    # which colors will be cleared (all here- without alpha) - every frame
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    glLoadIdentity()
+
+    # ttransle on x axis
+    glTranslate(0, -.025, 0)
+
+    # draw everything red
+    glColor3f(1, 0, 0)
+
+    # draw solid Sphere
+    # void glutSolidSphere(GLdouble radius, GLint slices, GLint stacks);
+    glutSolidSphere(.01, 200, 200)
+
+
+    # swap the Buffers on Projection Matrix
+    glutSwapBuffers()
+    return
+
+def keyFunc(key, x, y):
+    if key == ESCAPE:
+        sys.exit()
+
+def mouseFunc(key, mode, x, y):
+    if mode:
+        print x, y
+    pass
+
+def init():
+    # Init OpenGL Utility Toolkit
+    glutInit()
+    # Init the Display Mode
+    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE)
+    # and window size
+    glutInitWindowSize(width, height)
+    # and the Window Title (the b in front, is to give the name in bitwise - opengl needs that)
+    glutCreateWindow(b"HS-RM 3D-Animation Avalanche")
+
+    # enable if front and backface is rendered
+    glEnable(GL_CULL_FACE)
+
+    # enable depth Test
+    glEnable(GL_DEPTH_TEST)
+
+    # enable Lighting and Shadows @todo we need to add a ligth afterwards
+    # glEnable(GL_LIGHTING)
+
+    # enabling colored material
+    glEnable(GL_COLOR_MATERIAL)
+
+    # clear the screen
+    glClearColor(1, 1, 1, 0)
+
+    # to have a callback function we need to add a display function
+    glutDisplayFunc(drawLoop)
+
+    # callback for keystroke
+    glutKeyboardFunc(keyFunc)
+
+    # callback for mousepress
+    glutMouseFunc(mouseFunc)
+
+    # Idle function for the 60 fps callback
+    glutIdleFunc(drawLoop)
+
+    # create Projection Matrix
+    glMatrixMode(GL_PROJECTION)
+
+    # set up a perspective projection matrix
+    # void gluPerspective(	GLdouble fovy,	GLdouble aspect, GLdouble zNear, GLdouble zFar);
+    gluPerspective(40., (width / height), 1., 40.)
+
+    # define a viewing transformation - Camera on Z axis 10 away
+    # void gluLookAt(GLdouble eyeX, GLdouble eyeY, GLdouble eyeZ, GLdouble centerX, GLdouble centerY, GLdouble centerZ, GLdouble upX, GLdouble upY, GLdouble upZ);
+    gluLookAt(0, 0, 10,
+              0, 0, 0,
+              0, 1, 0)
+
+    # glutMainLoop enters the GLUT event processing loop. This routine should be called at most once in a GLUT program.
+    # Once called, this routine will never return. It will call as necessary any callbacks that have been registered.
+    glutMainLoop()
+
+
+if __name__ == '__main__':
+    init()
